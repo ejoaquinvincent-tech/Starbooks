b7edec47724288c96877240083b47bddf63d10cd
AltIsBacc <zackrondenzoolarte@gmail.com>
2026-02-26 14:40:27 +0000

    login!!!

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,10 @@
 const isLoggedIn = localStorage.getItem("isLoggedIn");
 
-if ( isLoggedIn === "true" ) {
+if (isLoggedIn === "true") {
     const loginButton = document.getElementById('sign-in-button'); // the id always available because navbar IS always present in each page
 
-    loginButton.innerHTML = "Signed";
-    loginButton.classList.replace('outlined-button', 'primary-button');
+    loginButton.innerHTML = "Account";
+    loginButton.classList.replace('positive-button', 'outlined-button');
 }
 
 // document.documentElement.setAttribute('ui-theme', 'dark');
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,40 @@
-function login(e) {
+
+function doLogin(e) {
     e.preventDefault();
 
+    console.log("we boutta log in!");
+
+    const emailField = document.getElementById("login-email").value;
+
+    // gets the index of '@' then
+    // split the string from the
+    // beginning until index of '@'
+    const name = emailField.substring(0, emailField.indexOf('@'));
+
     localStorage.setItem("isLoggedIn", "true");
+    localStorage.setItem("accountName", name);
 
     window.location.href = "main.html";
 }
+
+function doLogout() {
+    localStorage.setItem("isLoggedIn", "false");
+    localStorage.setItem("accountName", "");
+
+    window.location.href = "login.html";
+}
+
+function updateView() {
+    const loginForm = document.getElementById('login-form');
+    const alreadyLoggedIn = document.getElementById('already-logged-in');
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn === "true") {
+        alreadyLoggedIn.getElementsByTagName("h2")[0].innerHTML += ` ${localStorage.getItem('accountName')}`;
+        loginForm.style.display = 'none';  // hide login form
+    } else {
+        alreadyLoggedIn.style.display = 'none';  // hide "already logged in" card
+    }
+}
+
+updateView();
